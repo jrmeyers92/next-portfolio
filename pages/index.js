abb8dff203b35e50167bef919af79dbc9d880635
jrmeyers92 <JakeRMeyers92@gmail.com>
2023-02-02 08:45:48 -0600

    create head and meta tags

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 // import styles from "../styles/.module.css";
-
 // components
+import Head from "next/head";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import SocialIcons from "../components/SocialIcons";
@@ -13,6 +13,14 @@ import Footer from "../components/Footer";
 export default function Home() {
   return (
     <div className="font-inter mx-4">
+      <Head>
+        <title>Jake Meyers Portfolio</title>
+        <meta charset="UTF-8" />
+        <meta name="description" content="A Web Development portfolio for Jake Meyers" />
+        <meta name="keywords" content="Web Development, Software Developer, JavaScript, HTML, CSS, React, Node JS, Java, CoreMedia, Web Developer, Full Stack, Tailwind CSS, Bootstrap, Jquery" />
+        <meta name="author" content="Jake Meyers" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      </Head>
       <Navbar />
       <SocialIcons />
       <EmailFixedLink />
